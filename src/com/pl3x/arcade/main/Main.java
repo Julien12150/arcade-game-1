2f97f997d86622d11575d2942cce9deb0349a005
Julien12150 <julienfillol2@gmail.com>
2015-09-28 00:33:04 +0200

    fps
    made fps stuff and a black background
    and comments

--- src/com/pl3x/arcade/main/Main.java
+++ src/com/pl3x/arcade/main/Main.java
@@ -1,24 +1,88 @@
 package com.pl3x.arcade.main;
 
-import java.awt.Canvas;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Main extends Canvas implements Runnable{
 	
 	private static final long serialVersionUID = 6230533464412165714L;
 	
+	public static final int WIDTH = 750;
+	public static final int HEIGHT = 500;
+	
+	public static String name = "Arcade game"; //TODO: change the title
+	
+	private Thread thread;
+	private boolean isRunning = false;
+	
 	public Main(){
-		new Windows(750, 500, "Arcade Game", this); //TODO: change the title
+		new Windows(WIDTH, HEIGHT, name, this); //it's make a new Windows
 	}
 	
-	public synchronized void start(){
+	public synchronized void start(){ //when it's start
+		thread = new Thread(this);
+		thread.start();
+		isRunning = true;
+		System.out.println("Started");
+	}
+	public synchronized void stop(){
+		try{
+			thread.join();
+			isRunning = false;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void run(){ //many things about fps stuff
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(isRunning){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(isRunning)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	private void tick(){
 		
 	}
 	
-	public void run(){
+	private void render(){
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null){
+			this.createBufferStrategy(3);
+			return;
+		}
 		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0,  0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
-		new Main();
+		new Main(); //what happen when the program start? it's make a new "Main"
 	}
 }
--- src/com/pl3x/arcade/main/Windows.java
+++ src/com/pl3x/arcade/main/Windows.java
@@ -8,17 +8,17 @@ public class Windows extends Canvas {
 	private static final long serialVersionUID = -8681307114418070490L;
 	
 	public Windows(int width, int height, String title, Main main){
-		JFrame frame = new JFrame(title);
+		JFrame frame = new JFrame(title); //lets make a new frame!
 		
-		frame.setPreferredSize(new Dimension(width, height));
+		frame.setPreferredSize(new Dimension(width, height)); //width/height of the windows
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.add(main);
-		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //what happen when you click the X button? the program close!
+		frame.setResizable(false); //it's resizable? nah
+		frame.setLocationRelativeTo(null); 
+		frame.add(main); //it's add "main"
+		frame.setVisible(true); //its become visible!
 		main.start();
 	}
 	
